747e3b02d84da4a2243bebfdeec1a030f452b062
Tom Cant <tomcant@gmail.com>
2021-04-13 18:15:57 +0100

    Add debug output around priority sampling value within class StdOutJsonStream

--- php/DDTrace/Transport/StdOutJsonStream.php
+++ php/DDTrace/Transport/StdOutJsonStream.php
@@ -20,11 +20,23 @@ namespace DDTrace\Transport {
 
         public function send(Tracer $tracer)
         {
+            $traces = $this->normaliseTraces($tracer);
+            $isDebug = false !== (bool) \getenv('LOG_BUNDLE_SERVERLESS_DEBUG');
+
+            if ($isDebug) {
+                fwrite($this->stream, '[DEBUG] prioritySampling = ' . $tracer->getPrioritySampling() . PHP_EOL);
+                fwrite($this->stream, '[DEBUG] ' . json_encode($traces) . PHP_EOL);
+            }
+
             if (\in_array($tracer->getPrioritySampling(), [PrioritySampling::AUTO_REJECT, PrioritySampling::USER_REJECT])) {
+                if ($isDebug) {
+                    fwrite($this->stream, '[DEBUG] Dropping trace as requested'. PHP_EOL);
+                }
+
                 return;
             }
 
-            foreach ($this->normaliseTraces($tracer) as $trace) {
+            foreach ($traces as $trace) {
                 foreach (array_chunk($trace, self::SPAN_CHUNK_SIZE) as $spans) {
                     fwrite($this->stream, json_encode(['headers' => $this->headers, 'traces' => [$spans]]) . PHP_EOL);
                 }
